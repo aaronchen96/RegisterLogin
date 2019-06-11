33643b34581fc5701ee9de5297c9ea76fd1ae32a
aaronchen96 <aaron0521@live.com>
2019-06-12 00:35:00 +0800

    Initial commit

--- app/src/main/java/com/example/registerlogin/HomeActivity.java
+++ app/src/main/java/com/example/registerlogin/HomeActivity.java
@@ -1,7 +1,13 @@
 package com.example.registerlogin;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,6 +15,55 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_search_pet:
+                        Toast.makeText(HomeActivity.this, "Action Add Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_map:
+                        Toast.makeText(HomeActivity.this, "Action Add Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_add:
+                        Toast.makeText(HomeActivity.this, "Action Add Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.action_profile:
+                        Toast.makeText(HomeActivity.this, "Action Add Clicked", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return true;
+            }
+        });
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+      int id = item.getItemId();
+
+      if(id== R.id.action_back) {
+          Toast.makeText(getApplicationContext(), "Back clicked",
+                  Toast.LENGTH_SHORT).show();
+      }else if (id == R.id.action_chat){
+          Toast.makeText(getApplicationContext(), "Chat clicked",
+                  Toast.LENGTH_SHORT).show();
+
+    }
+    return true;
+}
+
+    private void setSupportActionBar(Toolbar toolbar) {
     }
 
 
