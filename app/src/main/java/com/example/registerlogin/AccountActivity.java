4aad1c68db852d2ebe88fb592f26b2870bab563d
JianHwa <swe1609512@xmu.edu.my>
2019-06-13 11:34:39 +0800

    delete testing.xml

--- app/src/main/java/com/example/registerlogin/AccountActivity.java
+++ app/src/main/java/com/example/registerlogin/AccountActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class AccountActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
